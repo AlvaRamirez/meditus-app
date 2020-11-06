3561e8de559416e49889f901ffb4bd4b8e8824ff
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-11-05 23:14:10 -0300

    ahora si

--- components/Header2/index.js
+++ components/Header2/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-scroll'
-import * as Scroll from 'react-scroll'
+//import { Link } from 'react-scroll'
+//import * as Scroll from 'react-scroll'
 import { default as NextLink } from 'next/link'
 import { useAuth } from 'lib/useUser';
 
--- pages/Experimenta.js
+++ pages/Experimenta.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Aporte from '../components/Aporte'
+import Header2 from '../components/Header2'
 
 const Experimenta = () => {
 
